f1b9f3c13b4c172bead747c358e8c1c96f3efff9
helenferminastacheski <113613461+helenferminastacheski@users.noreply.github.com>
2024-06-06 10:19:31 -0300

    ok

---
+++ script.js
@@ -0,0 +1,18 @@
+const caixaPrincipal = document.querySelector(".caixa-principal");
+const caixaPeguntas = document.querySelector(".caixa-perguntas");
+const caixaAlternativa = document.querySelector(".caixa-alternativa");
+const caixaResultado = document.querySelector(".caixa-resultado");
+const textoResultado = document.querySelector(".texto-principal");
+
+const perguntas = [
+   {
+    ecnunciado: "pergunta1",
+    alternativas: ["alternativa1", "alterntiva2"]
+   },
+   {
+    ecnunciado: "pergunta2",
+    alternativas: ["alternativa1", "alterntiva2"]
+   },
+];
+
+
